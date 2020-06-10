6ff7d83b0686c5cdf93dc4b792f096954ec56caf
aebolivar <angiealina01@gmail.com>
2020-06-10 10:04:39 -0500

    organizacionCarpetas

--- Principal.py
+++ Principal.py
@@ -1,5 +1,5 @@
 import pygame
-import player
+import guerrero
 
 pygame.init()
 
@@ -7,9 +7,9 @@ pygame.init()
 ancho_ventana = 640
 alto_ventana = 480
 screen = pygame.display.set_mode((ancho_ventana, alto_ventana))
-pygame.display.set_caption("Personaje")
+pygame.display.set_caption("Guerrero.png")
 clock = pygame.time.Clock()
-player = player.Kate((ancho_ventana/2, alto_ventana/2))
+guerrero = guerrero.Guerrero((ancho_ventana/2, alto_ventana/2))
 game_over = False
 
 while game_over == False:
@@ -18,9 +18,9 @@ while game_over == False:
         if event.type == pygame.QUIT:
             game_over = True
 
-    player.handle_event(event)
+    guerrero.handle_event(event)
     screen.fill(pygame.Color('gray'))
-    screen.blit(player.image, player.rect)
+    screen.blit(guerrero.image, guerrero.rect)
 
     pygame.display.flip()
     clock.tick(20)
